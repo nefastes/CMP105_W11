60456c2ce70090a55ea6314a268fcd1f7e66ba9b
nefastes <56042406+nefastes@users.noreply.github.com>
2020-03-26 18:07:56 +0100

    fixed collisions
    Finally fixed this thing damnit

---
+++ Week11/CMP105App/Menu.cpp
@@ -0,0 +1,15 @@
+#include "Menu.h"
+Menu::Menu(sf::RenderWindow* hwnd, Input* in, GameState* gs, AudioManager* aud)
+{
+	window = hwnd;
+	input = in;
+	gameState = gs;
+	audio = aud;
+
+	// initialise menu objects
+}
+
+Menu::~Menu()
+{
+
+}
